faf15b7a2ee6a7572f5d48f7665ccb1d5336fd6a
Orlando Parente <parenteorlando@yahoo.it>
2025-01-26 13:39:02 +0100

    .

--- local_peer_RTC/js/js_consts.js
+++
@@ -1,2 +0,0 @@
-// var signaling_server = "http://localhost/html/signaling_server/signaling_server.php" ;
-var signaling_server = "https://serversignaling.altervista.org/signaling_server/signaling_server.php" ;
